b11b8866960ffa00f43db342097a6eb505b5bc0c
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2017-05-28 23:24:47 -0400

    cleaned up load_table and new_db

--- brunch/load_table.py
+++ brunch/load_table.py
@@ -6,148 +6,155 @@ import sqlite3 as sql
 
 from optparse import OptionParser
 
-fld_dict = { 'filename': 'file',
-             '#pass-ing' : 'pass_ing'}
+class LoadTable (object):
+    def __init__ (self):
+        self.name = 'load_table'
+        self.help = 'Create a table based on csv'
+        self.fld_dict = { 'filename': 'file',
+                          '#pass-ing' : 'pass_ing'}
 
-def fld_name (fld):
-    if fld in fld_dict: return fld_dict [fld]
-    return fld.replace ('.', '_').replace (' ', '_')
+    def fld_name (self, fld):
+        if fld in self.fld_dict: return self.fld_dict [fld]
+        return fld.replace ('.', '_').replace (' ', '_')
 
-def get_type (v):
-    if v == None: return None
-    if v == '': return None
-    try:
-        int (v)
-        return 'integer'
-    except ValueError:
+    def get_type (self, v):
+        if v == None: return None
+        if v == '': return None
         try:
-            float (v)
-            return 'real'
+            int (v)
+            return 'integer'
         except ValueError:
-            return 'text'
+            try:
+                float (v)
+                return 'real'
+            except ValueError:
+                return 'text'
     
-def deduce_types (reader):
-    types = map (get_type, next (reader))
-    def merge_type (t1, t2):
-        if t1 == 'text' or t2 == 'text': return 'text'
-        elif t1 == 'real' or t2 == 'real': return 'real'
-        elif t1 == 'integer' or t2 == 'integer': return 'integer'
-        else: 
-            assert t1 == t2 == None
-            return None
+    def deduce_types (self, reader):
+        types = map (self.get_type, next (reader))
+        def merge_type (t1, t2):
+            if t1 == 'text' or t2 == 'text': return 'text'
+            elif t1 == 'real' or t2 == 'real': return 'real'
+            elif t1 == 'integer' or t2 == 'integer': return 'integer'
+            else: 
+                assert t1 == t2 == None
+                return None
 
-    for row in reader:
-        types = [merge_type (t, get_type (i)) for (t, i) in zip (types, row)]
-    return types
+        for row in reader:
+            types = [merge_type (t, self.get_type (i)) for (t, i) in zip (types, row)]
+        return types
     
-def normalize_fld_names (fld):
-    flds = dict ()
-    for i in range (len (fld)):
-        if flds.get (fld [i]) is not None:
-            n = flds [fld [i]] 
-            n = n + 1
-            flds [fld [i]] = n
-            # add index to fld name
-            fld [i] = fld[i] + str(n)
-        else:
-            flds [fld [i]] = 0
-    return fld
+    def normalize_fld_names (self, fld):
+        flds = dict ()
+        for i in range (len (fld)):
+            if flds.get (fld [i]) is not None:
+                n = flds [fld [i]] 
+                n = n + 1
+                flds [fld [i]] = n
+                # add index to fld name
+                fld [i] = fld[i] + str(n)
+            else:
+                flds [fld [i]] = 0
+        return fld
     
-def create_ufo_table (db, table, flds, types):
-    sql_fld_names = map (fld_name, flds)
-    sql_fld_types = map (lambda x: x == None and 'text' or x, types)
+    def create_ufo_table (self, db, table, flds, types):
+        sql_fld_names = map (self.fld_name, flds)
+        sql_fld_types = map (lambda x: x == None and 'text' or x, types)
 
-    sql_col_def = ', '.join ([' '.join ((x, y)) 
-                             for (x,y) in zip (sql_fld_names, sql_fld_types)])
+        sql_col_def = ', '.join ([' '.join ((x, y)) 
+                                for (x,y) in zip (sql_fld_names, sql_fld_types)])
+
+        print "Creating table '{table}'".format(table=table)
+
+        ctable = """
+    drop table if exists {name};
+    drop index if exists {name}_total;
+    drop index if exists {name}_file;
+    create	table if not exists {name}( {cols} );
+
+    create unique index if not exists {name}_file on {name}(file);
+
+    """
+        ctable = ctable.format (name=table, cols=sql_col_def)
+        db.cursor ().executescript (ctable);
+        db.commit ()
     
-    print "Creating table '{table}'".format(table=table)
-
-    ctable = """
-drop table if exists {name};
-drop index if exists {name}_total;
-drop index if exists {name}_file;
-create	table if not exists {name}( {cols} );
-
-create unique index if not exists {name}_file on {name}(file);
-
-"""
-    ctable = ctable.format (name=table, cols=sql_col_def)
-    db.cursor ().executescript (ctable);
-    db.commit ()
-    
-def load_ufo_csv (csvfile, db, table, create_table = False):
-    print "Loading file '{name}'".format (name=csvfile)
-    c = db.cursor ()
-    with open (csvfile, 'rb') as infile:
-        reader = csv.reader (infile, dialect='excel')
-        # read the header
-        fld = next (reader)
-        
-        fld = normalize_fld_names (fld)
-        col = ', '.join (['?' for i in fld])
-        ivalue = "insert into {name} values ({col});"
-        ivalue = ivalue.format (name=table, col=col)
-        
-        if create_table: 
-            types = deduce_types (reader)
-            create_ufo_table (db, table, fld, types)
-
-            ## reset everything to the beginning
-            infile.seek (0)
+    def load_ufo_csv (self, csvfile, db, table, create_table = False):
+        print "Loading file '{name}'".format (name=csvfile)
+        c = db.cursor ()
+        with open (csvfile, 'rb') as infile:
             reader = csv.reader (infile, dialect='excel')
-            x = next (reader)
+            # read the header
+            fld = next (reader)
 
-        c.executemany (ivalue, reader)
-    db.commit ()
+            fld = self.normalize_fld_names (fld)
+            col = ', '.join (['?' for i in fld])
+            ivalue = "insert into {name} values ({col});"
+            ivalue = ivalue.format (name=table, col=col)
+
+            if create_table: 
+                types = self.deduce_types (reader)
+                self.create_ufo_table (db, table, fld, types)
+
+                ## reset everything to the beginning
+                infile.seek (0)
+                reader = csv.reader (infile, dialect='excel')
+                x = next (reader)
+
+            c.executemany (ivalue, reader)
+        db.commit ()
     
-def create_solved_views (db, table):
-    print "Creating solved views"
-    view = """
-create view if not exists {name}_solved as select num, count(*) as solved from {name}, nums where (res is 'SAFE' or res is 'CEX') and total <= num group by num order by num;
+    def create_solved_views (self, db, table):
+        print "Creating solved views"
+        view = """
+    create view if not exists {name}_solved as select num, count(*) as solved from {name}, nums where (res is 'SAFE' or res is 'CEX') and total <= num group by num order by num;
 
-create view if not exists {name}_solved_safe as select num, count(*) as solved from {name}, nums where (res is 'SAFE') and total <= num group by num order by num;
+    create view if not exists {name}_solved_safe as select num, count(*) as solved from {name}, nums where (res is 'SAFE') and total <= num group by num order by num;
 
-create view if not exists {name}_solved_cex as select num, count(*) as solved from {name}, nums where (res is 'CEX') and total <= num group by num order by num;
+    create view if not exists {name}_solved_cex as select num, count(*) as solved from {name}, nums where (res is 'CEX') and total <= num group by num order by num;
 
-""".format (name=table)
+    """.format (name=table)
 
-    db.cursor ().executescript (view)
-    db.commit ()
+        db.cursor ().executescript (view)
+        db.commit ()
+        
+    def mk_arg_parser (self, ap):
+        ap.add_argument ('-t', '--table', dest='table',
+                         help='Table name',required=True)
+        ap.add_argument ('--db', dest='db',
+                         metavar='FILE', help='Database location',
+                         default='results.sqlite')
+        ap.add_argument ('--use-existing', dest='existing',
+                         default=False, help='Do not re-create the table',
+                         action='store_true')
+        ap.add_argument ('in_files', nargs='+', help='CSV files')
 
-def parseOpt ():
-    parser = OptionParser ()
-    parser.add_option ("-t", "--table", dest="table",
-                       help="Table name", metavar="TABLE");
-    parser.add_option ("--use-existing", action="store_true", dest="existing", 
-                       default = False, help = "Do not re-create the table")
-    parser.add_option ("--db", dest="db", 
-                       help="Database location",
-                       default="results.db")
+        return ap
 
-    (options, args) = parser.parse_args ()
+    def run (self, args):
+        db = sql.connect (args.db)
+        create_table = not args.existing
+        for file in args.in_files:
+            self.load_ufo_csv (file, db, args.table, create_table)
+            create_table = False
 
-    if len (args) < 1:
-        parser.error ("no file given")
+        #    create_solved_views (db, opt.table)
 
-    if options.table == None:
-        parser.error ("no table name")
+        db.close ()
 
-    return (options, args)
-    
+        
+    def main (self, argv):
+        import argparse
+        
+        ap = argparse.ArgumentParser (prog=self.name, description=self.help)
+        ap = self.mk_arg_parser (ap)
+
+        args = ap.parse_args (argv)
+        return self.run (args)
     
 def main ():
-
-    (opt, args) = parseOpt ()
-    db = sql.connect (opt.db)
-    create_table = not opt.existing
-    for file in args:
-        load_ufo_csv (file, db, opt.table, create_table)
-        create_table = False
-
-#    create_solved_views (db, opt.table)
-    
-    db.close ()
-
+    cmd = LoadTable ()
+    return cmd.main (sys.argv[1:])
 
 if __name__ == "__main__":
     sys.exit (main ())
--- brunch/new_db.py
+++ brunch/new_db.py
@@ -2,43 +2,51 @@
 
 import sys
 import sqlite3 as sql
-from optparse import OptionParser
 
+class NewDb (object):
 
-def create_num_table (db):
-    ctable = """
-drop table if exists nums;
-drop index if exists nums_num;
-create table if not exists nums (num integer);
-create unique index if not exists nums_num on nums(num);
-"""
-    curs = db.cursor ()
-    curs.executescript (ctable)
+    def __init__ (self):
+        self.name = 'new_db'
+        self.help = 'Create new sqlite database'
+        
+    def create_num_table (self, db):
+        ctable = """
+    drop table if exists nums;
+    drop index if exists nums_num;
+    create table if not exists nums (num integer);
+    create unique index if not exists nums_num on nums(num);
+    """
+        curs = db.cursor ()
+        curs.executescript (ctable)
 
-    ivalue = "insert into nums values (?);"
-    curs.executemany (ivalue, [ (i, ) for i in xrange (1, 120, 1)])
+        ivalue = "insert into nums values (?);"
+        curs.executemany (ivalue, [ (i, ) for i in xrange (1, 120, 1)])
 
-    db.commit ()
+        db.commit ()
+        
+    def mk_arg_parser (self, ap):
+        ap.add_argument ('--db', dest='db',
+                         metavar='FILE', help='Database location',
+                         default='results.sqlite')
+        return ap
 
-def parseOpt ():
-    parser = OptionParser ()
-    parser.add_option ("--db", dest="db", 
-                       help="Database location",
-                       default="results.db")
-    (opt, args) = parser.parse_args ()
-    if not len (args) == 0:
-        parser.error ("Unknown arguments")
+    def run (self, args):
+        db = sql.connect (args.db)
+        self.create_num_table (db)
+        db.close ()
+        
+    def main (self, argv):
+        import argparse
+        
+        ap = argparse.ArgumentParser (prog=self.name, description=self.help)
+        ap = self.mk_arg_parser (ap)
 
-    return opt
+        args = ap.parse_args (argv)
+        return self.run (args)
     
-
 def main ():
-    opt = parseOpt ()
+    cmd = NewDb ()
+    return cmd.main (sys.argv[1:])
 
-    db = sql.connect (opt.db)
-    create_num_table (db)
-    db.close ()
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit (main ())
